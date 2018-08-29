cd2d0b6b39d3d3024e45f92a3dbf8bb9f5b5dc69
sram <sram@LHHYDLA0619.LHCORP.liquidhub.com>
2018-08-29 18:13:32 +0530

    Github 1st push through STS

---
+++ src/main/java/com/srini/tutorial/spring/config/jenkinTest.java
@@ -0,0 +1,10 @@
+package com.srini.tutorial.spring.config;
+
+public class jenkinTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nothing in here.");
+	}
+
+}
